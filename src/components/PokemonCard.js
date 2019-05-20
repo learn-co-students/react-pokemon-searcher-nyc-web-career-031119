a84269cf609c3744a756bec9f1f3c747b3d5bd76
ezevin <ehzevin@gmail.com>
2019-05-20 09:26:03 -0400

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,25 +2,58 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+  state = {side:true}
+
+  toggleCard = () =>{
+    if (this.state.side === true){
+      this.setState({side:false})
+    }
+    else if (this.state.side === false)
+    { this.setState({side:true})}
+  }
+
   render() {
-    return (
-      <Card>
-        <div>
-          <div className="image">
-            <img alt="oh no!" />
+    const pokemon = this.props.pokemon
+    const hp = pokemon.stats[pokemon.stats.length - 1].value
+      if (this.state.side === false){
+        return (
+        <Card >
+        
+          <div onClick={this.toggleCard}>
+            <div className="image">
+              <img src={ pokemon.sprites.back} alt="oh no!" />
+            </div>
+            <div className="content">
+              <div className="header">{pokemon.name}</div>
+            </div>
+            <div className="extra content">
+              <span>
+                <i className="icon heartbeat red" />
+                hp: {hp}
+              </span>
+            </div>
           </div>
-          <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+        </Card>
+      )}
+      else {return (
+        <Card >
+          <div onClick={this.toggleCard}>
+            <div className="image">
+              <img src={ pokemon.sprites.front} alt="oh no!" />
+            </div>
+            <div className="content">
+              <div className="header">{pokemon.name}</div>
+            </div>
+            <div className="extra content">
+              <span>
+                <i className="icon heartbeat red" />
+                hp: {hp}
+              </span>
+            </div>
           </div>
-          <div className="extra content">
-            <span>
-              <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
-            </span>
-          </div>
-        </div>
-      </Card>
-    )
+        </Card>
+      )}
   }
 }
 
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,52 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+  state = {pokemon: [],
+            search: ""
+          }
+
+
+  componentDidMount(){
+    fetch(`http://localhost:3000/pokemon`)
+    .then(res => res.json())
+    .then(pokemon => {this.setState({pokemon})})
+  }
+
+  handleSearch = (e, {value}) => {
+    this.setState({search: value})
+    console.log(value)
+  }
+
+  handleForm = (form) => {
+    let newPokemon = {
+      name: form.name,
+      sprites: {
+        front: form.frontUrl,
+        back: form.backUrl
+      },
+      stats: [{
+        name: "hp",
+        value: form.hp
+      }]
+    }
+  this.setState({pokemon: [...this.state.pokemon, newPokemon]})
+  }
+
   render() {
+    // console.log(this.state.pokemon)
+    const filtered = this.state.pokemon.filter(pokemon => {
+      return pokemon.name.toLowerCase().includes(this.state.search.toLowerCase())
+    })
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={ _.debounce(this.handleSearch, 500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonForm addPokemon={this.handleForm}/>
         <br />
-        <PokemonForm />
+        <PokemonCollection pokemon={filtered}/>
+        <br />
       </div>
     )
   }
