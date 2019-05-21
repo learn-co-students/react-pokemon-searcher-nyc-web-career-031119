94d36c616672c5827dc059e088140c35668ea16c
Yevgeniy <yevgeniyerot@gmail.com>
2019-05-21 16:31:56 -0400

    done

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,34 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+  state = {
+    displayFront: true
+  };
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      displayFront: !prevState.displayFront
+    }))
+  };
+
   render() {
+    const {name, sprites: {back, front}} = this.props.pokemon;
+    const hp = this.props.pokemon.stats.find(stat => stat.name === 'hp').value;
+
     return (
       <Card>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img alt="oh no!" src={this.state.displayFront ? front : back} onClick={this.handleClick}/>
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {hp}
             </span>
           </div>
         </div>
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { Form } from 'semantic-ui-react'
 
 class PokemonForm extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props);
 
     this.state = {
       name: '',
@@ -13,16 +13,61 @@ class PokemonForm extends React.Component {
     }
   }
 
+  handleInputChange = e => {
+    console.log(this.state);
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  };
+
+  handleSubmit = e => {
+     e.preventDefault();
+
+    this.setState({
+      name: '',
+      hp: '',
+      frontUrl: '',
+      backUrl: ''
+    });
+
+     fetch('http://localhost:3000/pokemon',{
+       method: 'POST',
+       headers: {
+         Accept: 'application/json',
+         'Content-type': 'application/json'
+       },
+       body: JSON.stringify({
+         name: this.state.name,
+         stats: [
+           {
+             value: this.state.hp,
+             name:  'hp'
+           }
+         ],
+         sprites: {
+           front: this.state.frontUrl,
+           back: this.state.backUrl
+         }
+       })
+     })
+       .then(r => r.json())
+       .then(data => {
+         this.props.createPokemon(data);
+         console.log(data)
+       })
+
+  };
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input value={this.state.name} onChange={this.handleInputChange} fluid label="Name" placeholder="Name" name="name" />
+            <Form.Input value={this.state.hp} onChange={this.handleInputChange} fluid label="hp" placeholder="hp" name="hp" />
+            <Form.Input value={this.state.frontUrl} onChange={this.handleInputChange} fluid label="Front Image URL" placeholder="url" name="frontUrl" />
+            <Form.Input value={this.state.backUrl} onChange={this.handleInputChange} fluid label="Back Image URL" placeholder="url" name="backUrl" />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
